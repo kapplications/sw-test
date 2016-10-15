33a4c8db1179b5b195937ee25f723cabdc4d678a
keighty <k80.leonard@gmail.com>
2016-10-15 09:14:27 -0700

    WIP

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 // register service worker
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
 
-    if(reg.installing) {
-      console.log('Service worker installing');
-    } else if(reg.waiting) {
-      console.log('Service worker installed');
-    } else if(reg.active) {
-      console.log('Service worker active');
-    }
-
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
+  addEventListener('install', function(event) {
+    console.log('Service worker installed.')
   });
+
+  addEventListener('activate', function (event) {
+    console.log('Service worker activated.')
+  })
+
+  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' })
+    .then(function(reg) {
+      console.log('Service worker registered.')
+    }).catch(function(error) {
+      console.log('Registration failed with ' + error);
+    });
 }
 
 // function for loading each image via XHR
